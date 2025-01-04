5711dd24648c40da6ac75c836d61d1222a0f77b3
Roel <roel@gethippo.app>
2025-01-04 19:50:48 +0100

    Initial Commit

---
+++ Generative/GenerativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerativeApp.swift
+//  Generative
+//
+//  Created by Roel van der Kraan on 04/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct GenerativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GenerativeTests/GenerativeTests.swift
@@ -0,0 +1,17 @@
+//
+//  GenerativeTests.swift
+//  GenerativeTests
+//
+//  Created by Roel van der Kraan on 04/01/2025.
+//
+
+import Testing
+@testable import Generative
+
+struct GenerativeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
