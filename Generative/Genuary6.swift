f2ac9253c0373a3744250fb0e876880c7c2bd606
Roel <mail@roelvanderkraan.nl>
2025-01-06 18:47:19 +0100

    Adds animation

--- Generative/Genuary6.swift
+++ Generative/Genuary6.swift
@@ -1,46 +1,41 @@
-//
-//  Genuary6.swift
-//  Generative
-//
-//  Created by Roel van der Kraan on 05/01/2025.
-//
-
 import SwiftUI
 
 struct Genuary6: View {
     var columns = 8
     var columnSpacer = -80.0
-
+    @State private var redrawTrigger = UUID() // Triggers a redraw when updated
+    
     var body: some View {
-        var triangleSize = 1000.0
-        
-//        TimelineView(.animation) { timeline in
-//            let now = timeline.date.timeIntervalSinceReferenceDate
+        GeometryReader { geometry in
             Canvas { context, size in
-                var centerY = size.height - triangleSize
-
+                var triangleSize = 1000.0
+                
                 triangleSize = 900.0
                 let backColor = color(red: 220, green: 213, blue: 235)
                 drawMountains(context: &context, size: size, triangleSize: triangleSize, color: backColor)
                 
                 triangleSize = 800.0
-                centerY = size.height - triangleSize
                 let thirdColor = color(red: 167, green: 176, blue: 218)
                 drawMountains(context: &context, size: size, triangleSize: triangleSize, color: thirdColor)
                 
                 triangleSize = 600.0
-                centerY = size.height - triangleSize
                 let secondColor = color(red: 1, green: 97, blue: 83)
                 drawMountains(context: &context, size: size, triangleSize: triangleSize, color: secondColor)
-            
+                
                 triangleSize = 300.0
-                let frontColor = color(red: 0, green: 87, blue: 83)
+                let frontColor = color(red: 0, green: 75, blue: 72)
                 drawMountains(context: &context, size: size, triangleSize: triangleSize, color: frontColor)
-   
-//            }
+            }
+            .id(redrawTrigger) // Causes the Canvas to redraw when updated
+            .onAppear {
+                // Start a timer to trigger redraw every 5 seconds
+                Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { _ in
+                    redrawTrigger = UUID() // Change the ID to force a redraw
+                }
+            }
+            .ignoresSafeArea()
+            .background(color(red: 174, green: 220, blue: 245))
         }
-        .ignoresSafeArea()
-        .background(color(red: 174, green: 220, blue: 245))
     }
     
     private func color(red: Int, green: Int, blue: Int) -> Color {
@@ -54,7 +49,7 @@ struct Genuary6: View {
     private func drawMountains(context: inout GraphicsContext, size: CGSize, triangleSize: Double, color: Color) {
         let centerY = size.height - triangleSize
         let xOffset = Double.random(in: 40...600)
-        for loopCount in (0..<columns) {
+        for loopCount in 0..<columns {
             let centerX = -xOffset + (Double(loopCount) * triangleSize * 0.8 - columnSpacer)
             let height = triangleSize * Double.random(in: 0.4...1)
             drawTriangle(context: &context, size: CGSize(width: triangleSize, height: height), origin: CGPoint(x: centerX, y: centerY + (triangleSize - height)), color: color)
@@ -62,7 +57,6 @@ struct Genuary6: View {
     }
     
     private func drawTriangle(context: inout GraphicsContext, size: CGSize, origin: CGPoint, color: Color) {
-        
         let rect = CGRect(origin: origin, size: size)
         let triangle = Triangle()
         let trianglePath = triangle.path(in: rect)
