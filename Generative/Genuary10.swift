5ff7ed10e700863b05bacd61c5e99002606ab502
Roel <mail@roelvanderkraan.nl>
2025-01-10 14:28:16 +0100

    Animated

--- Generative/Genuary10.swift
+++ Generative/Genuary10.swift
@@ -11,25 +11,26 @@ struct Genuary10: View {
     @State private var redrawTrigger = UUID() // Triggers a redraw when updated
     
     var tau = Double.pi * 2 // 6.2831855
+    @State var increaseTau = Double.pi * 2
     
     var body: some View {
         var single = tau / tau
         var halfTau = tau / single + single
-
+        
         ZStack {
-            Text("Genuary 10")
             Canvas { context, size in
-                drawCircle(in: &context)
+                increaseTau = halfTau / halfTau
                 var columnXStart = tau
                 var columnYStart = tau
                 for column in Int(tau)..<Int(tau * tau * tau) {
-                    for angle in stride(from: tau, to: tau*tau*tau*tau, by: halfTau) {
+                    for angle in stride(from: tau, to: increaseTau*tau, by: halfTau) {
                         var x = columnXStart + sin(angle) * tau
                         var y = columnYStart + cos(angle) * tau
                         let rect = CGRect(origin: CGPoint(x: x, y: y), size: CGSize(width: halfTau, height: tau*tau)).applying(.init(rotationAngle: angle))
                         let circle = Ellipse()
                         let circlePath = circle.path(in: rect)
-                        context.fill(circlePath, with: .color(.black))
+                        context.opacity = CGFloat.random(in: single/tau/tau...single/tau)
+                        context.fill(circlePath, with: .color(.blue))
                     }
                     columnXStart += tau + Double.random(in: single...halfTau)
                     columnYStart = tau
@@ -38,14 +39,13 @@ struct Genuary10: View {
             }
         }
         .ignoresSafeArea()
+        .onAppear {
+            // Start a timer to trigger redraw every 5 seconds
+            Timer.scheduledTimer(withTimeInterval: tau/tau/tau, repeats: true) { _ in
+                increaseTau += halfTau
+            }
+        }
         
-    }
-    
-    func drawCircle(in context: inout GraphicsContext) {
-        let rect = CGRect(origin: CGPoint(x: tau, y: tau), size: CGSize(width: tau, height: tau))
-        let circle = Circle()
-        let circlePath = circle.path(in: rect)
-        context.fill(circlePath, with: .color(.black))
     }
 }
 
